a5236e37d2a77a4b3efaaf364fa1a0b8852c6874
pokiuwu <72038114+pokiuwu@users.noreply.github.com>
2020-12-16 15:52:37 +0200

    Update EntryPoint.cpp

--- Client176/EntryPoint.cpp
+++ Client176/EntryPoint.cpp
@@ -1,49 +1,12 @@
-//---------------------------------------------------------------------------------------------
-// v203.4 Localhost Enabler - Rajan
-//---------------------------------------------------------------------------------------------
-
 #include "Global.h"
 
 
-Application app;
 
 VOID WINAPI HeaderProc(DWORD dwPID)
 {
 
 }
 
-
-void UpdateActivityCallback(void* data, EDiscordResult result)
-{
-    DISCORD_REQUIRE(result);
-}
-
-void DiscordLoop()
-{
-	Log(__FUNCTION__);
-
-    memset(&app, 0, sizeof(app));
-    
-    IDiscordActivityEvents activities_events;
-    memset(&activities_events, 0, sizeof(activities_events));
-
-    DiscordCreateParams params;
-    DiscordCreateParamsSetDefault(&params);
-    params.client_id = CLIENT_ID;
-    params.flags = DiscordCreateFlags_Default;
-    params.event_data = &app;
-    params.activity_events = &activities_events;
-    DISCORD_REQUIRE(DiscordCreate(DISCORD_VERSION, &params, &app.core));
-
-    app.activities = app.core->get_activity_manager(app.core);
-
-
-    for (;;) 
-    {
-        DISCORD_REQUIRE(app.core->run_callbacks(app.core));
-        Sleep(16);
-    }
-}
 
 DWORD WINAPI MainProc(PVOID)
 {
